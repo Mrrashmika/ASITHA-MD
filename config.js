894440eab99579d2f1380ac2467526acd0129ee1
BOT_RASHMIKA <janith60022006@gmail.com>
2024-10-02 17:54:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",//enter mongo db url
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=7QdQXJ4T#_MhNje-cRG4budhtWZ41II1hevnFzY2K4cq8kl9dCJ4",
+MONGODB: process.env.MONGODB || "mongodb://mongo:ypiPXBRszwKoMcXtwNJKRfSRVElHkAUS@junction.proxy.rlwy.net:39391",//enter mongo db url
 };
